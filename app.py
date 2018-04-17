990570c8491a7e2e6c736e6ffe1cc363ea3274d4
agrgn42 <angela.schopke@gmail.com>
2018-04-16 20:32:16 -0400

    pushing

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template, request, redirect
 import model
 
@@ -6,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     ## print the guestbook
-    return render_template("index.html", entries=model.get_entries())
+    return render_template("admin.html", entries=model.get_entries())
 
 @app.route("/add")
 def addentry():
@@ -18,8 +19,51 @@ def postentry():
     name = request.form["name"]
     message = request.form["message"]
     model.add_entry(name, message)
+    return redirect("/")    # redirects to a given page
+
+
+@app.route("/deleteentry", methods=["POST"])
+def deleteentry():
+    post_id = request.form["id"]
+    model.delete_entry(post_id)
     return redirect("/")
+
 
 if __name__=="__main__":
     model.init()
     app.run(debug=True)
+
+
+
+# with gunicorn:
+# gunicorn app:app - looks inside object called app to run app.py file
+# heroku communicates with git
+# Create a `requirements.txt` file.
+# Create a `runtime.txt` file. This will tell Heroku what version of Python to run.
+# run `python --version` to find out what version you’re running
+# write a single line into your `runtime.txt` file that looks like this (using your python version): python-3.6.4
+# Create a `Procfile`. This is a file that will tell Heroku how to run your app. Again, just one line: web: gunicorn app:app --log-file=-
+# Create a git repository in your project directory
+# First, create a .gitignore and put in it: 
+#   venv 
+#   __pycache__
+# git init 
+# git add .
+# git commit -m "first commit"
+
+# heroku login
+# heroku create
+
+# remote -v
+# heroku  https://git.heroku.com/tranquil-ravine-49028.git (fetch)
+# heroku  https://git.heroku.com/tranquil-ravine-49028.git (push)
+# git push heroku master
+
+# git remote add origin https://github.com/agrgn42/herokutest.git
+
+# now you can either "git push origin master" to update code, and then "git push heroku master" to deploy to the world
+
+
+
+
+
--- model.py
+++ model.py
@@ -1,31 +1,34 @@
+
 import json
 from datetime import datetime
 
-
 GUESTBOOK_ENTRIES_FILE = "entries.json"
 entries = []
+next_id = 0
 
-def init(app):
-    global entries
+
+def init():
+    global entries, next_id
     try:
-
         f = open(GUESTBOOK_ENTRIES_FILE)
         entries = json.loads(f.read())
+        next_id = len(entries)
         f.close()
     except:
-        print('Couldn\'t open', GUESTBOOK_ENTRIES_FILE)
         entries = []
+        next_id = 0
 
 def get_entries():
     global entries
     return entries
 
 def add_entry(name, text):
-    global entries, GUESTBOOK_ENTRIES_FILE
-    now = datetime.now()
-    time_string = now.strftime("%b %d, %Y %-I:%M %p")
-    entry = {"author": name, "text": text, "timestamp": time_string}
+    global entries, GUESTBOOK_ENTRIES_FILE, next_id
+    now = datetime.now()    # this gives you the time now
+    time_string = now.strftime("%b %d, %Y %-I:%M %p")   # this is s string formatting function, -I is the time now without the starting 0
+    entry = {"author": name, "text": text, "timestamp": time_string, "id": str(next_id)}
     entries.insert(0, entry) ## add to front of list
+    next_id +=1
     try:
         f = open(GUESTBOOK_ENTRIES_FILE, "w")
         dump_string = json.dumps(entries)
@@ -33,3 +36,18 @@ def add_entry(name, text):
         f.close()
     except:
         print("ERROR! Could not write entries to file.")
+
+
+def delete_entry(post_id):
+    global entries
+    for item in entries:
+        if post_id in item.values() or int(post_id) in item.values():
+            entries.remove(item)
+    entries_dict = json.dumps(entries)
+    fwrite = open(GUESTBOOK_ENTRIES_FILE, 'w')
+    fwrite.write(entries_dict)
+    fwrite.close()
+
+
+
+
